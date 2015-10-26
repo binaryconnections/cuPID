f3d5e561d6854c97ee8b614f7c513802d858fe1c
admin <usmansahir@github.com>
2015-10-26 19:39:55 -0400

    Usman is learning about version control and sharing with friends

---
+++ Usman Has a Small Penis/student.h
@@ -0,0 +1,17 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+class Student : public User
+{
+public:
+    Student();
+
+
+private:
+    // student has preferences
+    // has own attributes
+    // has projects registered in
+
+};
+
+#endif // STUDENT_H
