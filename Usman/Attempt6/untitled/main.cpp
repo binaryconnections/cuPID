35d011a13b7e42e199904410cfa90c9e8b513df7
doseofyous <youssef.halwani@gmail.com>
2015-11-03 22:41:46 -0500

    Cleaned files

--- Usman/Attempt6/untitled/main.cpp
+++ Usman/Attempt6/untitled/main.cpp
@@ -6,10 +6,10 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
 
     uiControl *uic = new uiControl();
-   // uic->wp->show();    // welcome page
+    uic->wp->show();    // welcome page
 
-    uic->sp->show();
-    uic->ap->show();
+    //uic->sp->show();
+    //uic->ap->show();
 
 
     return a.exec();
