3a1913a5cf0795eb499b8dfbd0912c7775806b4a
doseofyous <youssef.halwani@gmail.com>
2015-11-30 22:03:19 -0500

    Add control

---
+++ Bhargav/ControlClasses/storagecontrol.cpp
@@ -0,0 +1,6 @@
+#include "storagecontrol.h"
+
+storageControl::storageControl()
+{
+
+}
---
+++ Bhargav/ControlClasses/storagecontrol.h
@@ -0,0 +1,18 @@
+#ifndef STORAGECONTROL_H
+#define STORAGECONTROL_H
+
+#include "storage.h"
+using namespace std;
+
+class storageControl
+{
+public:
+    storageControl();
+
+    storage *data;  // has storage
+
+
+
+};
+
+#endif // STORAGECONTROL_H
